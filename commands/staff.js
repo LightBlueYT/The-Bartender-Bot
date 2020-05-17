deb230a2fd44da53297b1aebffdd89b8c32e2c72
Glitch (yellow-soft-streetcar) <none>
2020-05-17 20:06:32 +0000

    Alpha Alpha Three-Two

--- commands/staff.js
+++ commands/staff.js
@@ -11,25 +11,30 @@ module.exports = {
       
       async function arr() {
         const array = []
+        
             
         await message.guild.members.cache.forEach(m => {
+          const key = message.guild.id+m.id;
           if(client.staff.has(message.guild.id + m.id)) {
+            const perms = []
+            if(client.staff.get(key, 'allow_kick')) perms.push('KICK_MEMBERS')
+            if(client.staff.get(key, 'allow_ban')) perms.push('BAN_MEMBERS')
+            if(client.staff.get(key, 'allow_mute')) perms.push('MUTE_MEMBERS')
+            if(client.staff.get(key, 'allow_warn')) perms.push('WARN_MEMBERS')
             if(client.staff.get(message.guild.id+m.id, 'isStaff')){
-              array.push(m.displayName)
+              array.push(`${m.displayName}: ${perms.join(', ')}`)
             }
           } 
         })
         if(array.length <= 0) return message.channel.send('There is no custom staff in this server')
-        message.channel.send(`${array.join('  |  ')}`)
+        message.channel.send(`\`\`\`${array.join('\n')}\`\`\``)
       }
       arr()
     }
     
     let target = message.mentions.members.first() || message.guild.members.cache.get(args[1]) || message.guild.members.cache.find(m => m.user.tag === args[1])
     
-    if(args[0] === 'add'){
-      if(client.staff.get(message.guild.id+target.id, 'isStaff')) return message.channel.send(`This member is already in the STAFF Team`)
-      
+    if(args[0] === 'add'){      
       if(!client.staff.has(message.guild.id+target.id)){
         client.staff.ensure(message.guild.id + target.id, {
           member: target.id,
@@ -40,6 +45,8 @@ module.exports = {
           allow_warn: false
         });
       }
+      if(client.staff.get(message.guild.id+target.id, 'isStaff')) return message.channel.send(`This member is already in the STAFF Team`)
+      
       client.staff.set(message.guild.id + target.id, true, 'isStaff')
       message.channel.send(`${target.displayName} is now a part of the STAFF Team\nTo set permissions use <prefix>staff permissions <member> <permission> true/false`)
     }
