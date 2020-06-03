bba5314a0254f2733fba4111cfd53cf67324de8d
LightBlueYT <34584672+LightBlueYT@users.noreply.github.com>
2020-06-03 21:16:33 +0200

    Update docs.js

--- commands/docs.js
+++ commands/docs.js
@@ -1,6 +1,6 @@
 const {red, green} = require('../colors.json');
 const fetch = require('node-fetch');
-let valid_projects = ['stable', 'master', 'rpc', 'commando', 'akairo', 'akairo-master'];
+let valid_projects = ['stable', 'master', 'rpc', 'commando', 'akairo', 'akairo-master', 'collection'];
 let validateProject = (p)=>valid_projects.includes(p);
 module.exports = {
 	name: 'docs',
@@ -12,8 +12,8 @@ module.exports = {
   perms_needed: [],
 	async execute(message, args, client, MessageEmbed) {
       let queryString = args[0]
-        let project = "master"
-        if(args[1] && args[1].startsWith('=src')) [, project]= args[1].split('=').join('--');
+        let project = "stable"
+        if(args[1] && args[1].startsWith('--src=')) [, project]= args[1].split('=');
         if(!validateProject(project)) return message.channel.send('Invalid source.')
         let docs = await fetch(`https://djsdocs.sorta.moe/v2/embed?src=${project}&q=${queryString}`).then(r => r.json())
         return await message.channel.send({embed: docs})
